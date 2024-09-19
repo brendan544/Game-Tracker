bc494fe330bca85f12e045a0ca1464381e6de1c7
brendan544 <brendan.gwer@student.moringaschool.com>
2024-09-19 18:13:46 +0300

    updated helpers.py

--- lib/db/models.py
+++ lib/db/models.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
 from sqlalchemy import Sequence
 
-# Define the base class for declarative models
+
 Base = declarative_base()
 
 class Player(Base):
